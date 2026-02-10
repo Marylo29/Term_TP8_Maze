7068f39ac62d409d2889fc039ceb61df8e213514
COLLIN Nicolas <collin-n@saint-louis29.net>
2026-02-10 16:22:07 +0100

    Génération de base du labyrinthe

--- src/sl29/games/maze.py
+++ src/sl29/games/maze.py
@@ -1,5 +1,5 @@
 """Module providing the Maze class"""
-
+import plotly.graph_objects as go
 import networkx as nx
 import random
 
@@ -22,9 +22,72 @@ class Maze:
         # 3. Remove all edges to start with a maze full of walls
         self.graph.remove_edges_from(list(self.graph.edges()))
 
+        self.create_maze()
+
+    def create_maze(self):
+        def _recur_maze(graph,long,lar,haut):
+            if long == 1 and lar == 1 and haut == 1:
+                return graph
+            else:
+                nodes = graph.nodes
+                nodes_list = sorted([node for node in nodes])
+                min_z,min_y,min_x = nodes_list[0]
+                if long >= lar and long >= haut:
+                    # Division
+                    nodes_a = [node for node in nodes if node[2] < (min_x + (long // 2))]
+                    nodes_b = [node for node in nodes if node[2] >= (min_x + (long // 2))]
+                    # Sous graphes
+                    graph1 = graph.subgraph(nodes_a).copy()
+                    graph2 = graph.subgraph(nodes_b).copy()
+                    # Appels récursifs
+                    graph1 = _recur_maze(graph1,(long//2),lar,haut)
+                    graph2 = _recur_maze(graph2,((long//2)+long%2),lar,haut)
+                    # Fusion et ajout du passage
+                    fused_graph = nx.compose(graph1, graph2)
+                    fused_graph.add_edge((min_z,min_y,min_x + (long//2)-1),(min_z,min_y,min_x + (long//2)))
+                    return fused_graph
+                if lar >= long and lar >= haut:
+                    # Division
+                    nodes_a = [node for node in nodes if node[1] < (min_y + (lar // 2))]
+                    nodes_b = [node for node in nodes if node[1] >= (min_y + (lar // 2))]
+                    # Sous graphes
+                    graph1 = graph.subgraph(nodes_a).copy()
+                    graph2 = graph.subgraph(nodes_b).copy()
+                    # Appels récursifs
+                    graph1 = _recur_maze(graph1,long,(lar//2),haut)
+                    graph2 = _recur_maze(graph2,long,((lar//2)+lar%2),haut)
+                    # Fusion et ajout du passage
+                    fused_graph = nx.compose(graph1, graph2)
+                    fused_graph.add_edge((min_z,min_y + (lar//2)-1,min_x),(min_z,min_y + (lar//2),min_x))
+                    return fused_graph
+                else:
+                    return graph
+        
+        self.graph = _recur_maze(self.graph,self.width,self.length,self.height)
+
 class Cell:
     """Class representing a cell in the maze"""
 
     def __init__(self, coords):
         """Initialize the cell with its coordinates"""
         self.coords = coords #(x,y,z)
+
+
+def plot_interactive_3d(maze):
+    edge_x, edge_y, edge_z = [], [], []
+    
+    for edge in maze.graph.edges():
+        edge_x.extend([edge[0][0], edge[1][0], None])
+        edge_y.extend([edge[0][1], edge[1][1], None])
+        edge_z.extend([edge[0][2], edge[1][2], None])
+
+    fig = go.Figure(data=[go.Scatter3d(
+        x=edge_x, y=edge_y, z=edge_z,
+        mode='lines',
+        line=dict(color='black', width=4)
+    )])
+    fig.show()
+
+if __name__ == '__main__':
+    maze = Maze(16,16)
+    plot_interactive_3d(maze)
